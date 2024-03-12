078479f672be5701f7c8823833affe5f1c8fb9eb
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-03-13 00:57:18 +0200

    0x01 task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    y = ''
     for i in str:
         if ord(i) in range(ord('a'), ord('z')):
             z = ord(i) - 32
             y = y + "{:c}".format(z)
-            print(y)
         else:
-            print(i)
+            y = y + i
+    print(y)
