b5616532b87d9d93055b39c937282b3abcc3583f
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-03-20 22:24:59 +0200

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string: str):
-    if roman_string is None or type(roman_string) != str:
+def _subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
+
+def roman_to_int(roman_string):
+    if not roman_string:
         return 0
-    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = [data[x] for x in roman_string] + [0]
-    rep = 0
 
-    for i in range(len(numbers) - 1):
-        if numbers[i] >= numbers[i+1]:
-            rep += numbers[i]
-        else:
-            rep -= numbers[i]
+    if not isinstance(roman_string, str):
+        return 0
 
-    return rep
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(roman_d.keys())
+
+    num = 0
+    last_rom = 0
+    numbers = [0]
+
+    for _char in roman_string:
+        for roman_letter in list_keys:
+            if roman_letter == _char:
+                if roman_d.get(_char) <= last_rom:
+                    num += _subtract(numbers)
+                    numbers = [roman_d.get(_char)]
+                else:
+                    numbers.append(roman_d.get(_char))
+
+                last_rom = roman_d.get(_char)
+
+    num += _subtract(numbers)
+
+    return (num)
