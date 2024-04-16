6c10a454a90f1a3763685a211d0851840a4499e8
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-04-16 20:17:57 +0200

    all tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,7 @@
 import sys
 
 if __name__ == "__main__":
+    """Add all arguments to a Python list and save them to a file."""
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('8-load_from_json_file').load_from_json_file
