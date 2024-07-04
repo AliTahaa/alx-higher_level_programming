8487ed83213a070d5114295f865339b170823432
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-05 02:42:32 +0300

    task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# respond with a message containing You got me! in the body of the response
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+# breaking to http protocols on school servers
+curl -s -L -X PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
