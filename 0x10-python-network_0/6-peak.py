553bed2e77184f124c46af1b82b00e1a3286f023
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-05 02:49:03 +0300

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,9 +3,24 @@
 
 
 def find_peak(list_of_integers):
-    """  """
-    max_id = None
-    for element in list_of_integers:
-        if max_id is None or max_id < element:
-            max_id = element
-    return max_id
+    """ function """
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 0:
+        return (None)
+    elif size == 1:
+        return (list_of_integers[0])
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid_id = int(size/2)
+    pek = list_of_integers[mid_id]
+    thelist = list_of_integers
+    if pek > thelist[mid_id - 1] and pek > thelist[mid_id + 1]:
+        return pek
+    elif pek < thelist[mid_id - 1]:
+        return find_peak(thelist[:mid_id])
+    else:
+        return find_peak(thelist[mid_id + 1:])
