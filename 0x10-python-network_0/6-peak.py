321039ef70da785baa3ba5b1c8df0f353ecdd28a
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-05 02:34:20 +0300

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,11 @@
 #!/usr/bin/python3
+""" finding peak in list of ints """
 
 
 def find_peak(list_of_integers):
-
-    if list_of_integers is None or len(list_of_integers) == 0:
-        return None
-
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-
-    mid_id = int(len(list_of_integers) / 2)
-
-    if mid_id != len(list_of_integers) - 1:
-        if list_of_integers[mid_id - 1] < list_of_integers[mid_id] \
-             and list_of_integers[mid_id + 1] < list_of_integers[mid_id]:
-            return list_of_integers[mid_id]
-    else:
-        if list_of_integers[mid_id - 1] < list_of_integers[mid_id]:
-            return list_of_integers[mid_id]
-        else:
-            return list_of_integers[mid_id - 1]
-
-    if list_of_integers[mid_id - 1] > list_of_integers[mid_id]:
-        a_list = list_of_integers[0:mid_id]
-    else:
-        a_list = list_of_integers[mid_id + 1:]
-
-    return find_peak(a_list)
+    """  """
+    max_id = None
+    for element in list_of_integers:
+        if max_id is None or max_id < element:
+            max_id = element
+    return max_id
