631baffd0b1f29eccbac96c02eb3c30af966f7b3
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-06 01:16:00 +0300

    t 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,5 +9,5 @@ from sys import argv
 if __name__ == '__main__':
     url = 'https://api.github.com/users/{}'.format(argv[1])
     req = requests.get(url,
-                       auth=HTTPBasicAuth(argv[1], argv[2]))
+                       auth=(argv[1], argv[2]))
     print(req.json().get('id'))
