3d181517d42f0a5d7a55bd00733c8a3c692c82af
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-30 23:34:04 +0300

    all tasks

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const req = require('req');
+
+req.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
