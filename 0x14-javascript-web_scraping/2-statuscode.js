5d56bb2b86a67b8c58c8cbdc69d11ed2e43a426b
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-07-30 23:44:49 +0300

    try

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const req = require('req');
+const request = require('request');
 const url = process.argv[2];
 
-req(url, function (err, response, body) {
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
